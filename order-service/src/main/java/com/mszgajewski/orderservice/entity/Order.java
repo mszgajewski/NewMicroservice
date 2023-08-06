25357342aef3dfe1ab5945f8cb8e90b9cd6a4045
mszgajewski <mszgajewski@gmail.com>
2023-08-06 22:23:16 +0200

    Initial Commit

--- order-service/src/main/java/com/mszgajewski/orderservice/entity/Order.java
+++ order-service/src/main/java/com/mszgajewski/orderservice/entity/Order.java
@@ -3,7 +3,6 @@ package com.mszgajewski.orderservice.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
